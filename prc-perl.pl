c7016756e78dc7a95c98fa7b25b7f3b40c107e19
kenosuda4 <rs7ten@gmail.com>
2020-11-18 21:52:35 +0900

    int

--- prc-perl.pl
+++ prc-perl.pl
@@ -1,5 +1,24 @@
 use strict; # 文法チェック
-use warnings; # 警告
+use warnings; # 警告を出す
 
-print "Hello Perl\n"
+# 変数　スカラー変数 配列、ハッシュ
+# スカラー  単一の値:　数値、文字列
+my $name;
+$name = "nosuke\n";
 
+my $age = 27;
+
+my $hight = 170.2;
+my $big = 123_456_789
+# 変数指定する前に処理を記述するとエラーが起きる
+
+print "私は$nameです\n";
+
+
+print "$age\n";
+
+print "Hello Perl\n";
+
+print "$hight \n";
+
+print "$big \n";
