eb9eca30fc4b084ba774af2e8c34d1fc8a745b58
kenosuda4 <rs7ten@gmail.com>
2020-11-22 21:18:41 +0900

    if

---
+++ prc-hash.pl
@@ -0,0 +1,10 @@
+use strict;
+use warnings;
+
+# ハッシュ変数 キーと値: %
+my %bests = ("natu"=>28.00, "tyori"=>32.43, "kazu"=>25.53, "nosu"=> 24.89);
+
+print %bests;
+print "\n";
+
+print $bests{"natu"}. "\n";
