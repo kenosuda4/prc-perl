3a2e677dda6d6703e991a95f0d4e5d91b04dbf2d
kenosuda4 <rs7ten@gmail.com>
2020-11-23 20:42:40 +0900

    hash repe

--- prc-repetition.pl
+++ prc-repetition.pl
@@ -5,13 +5,28 @@ my $i = 0;
 # while文
 while ($i < 10 ){
     print "i = $i\n";
-
-
     $i++;
 }
 
 # for文
-
+# 同じファイル内に変数iがあるので最初に初期化処理
 for (my $i = 0; $i < 10; $i++){
     print "$i回目\n";
+}
+
+# foreach文
+my @colors = qw(red blue green orange pink);
+foreach my $color (@colors){
+    print "color = $color\n"
+}
+# foreachのeachは省略可能 上と下の出離力結果は同じ
+# my @colors = qw(red blue green orange pink);
+# for my $color (@colors){
+#     print "color = $color\n"
+# }
+
+# スカラー変数は省略可能 $_に配列の中身が自動的に代入される
+my @colors = qw(red blue green orange pink);
+for (@colors){
+    print "color = $_\n"
 }
